30af35a1667fba503a6b60a3f9d4b537d9b6921a
Mateo Barona <theflacorex@gmail.com>
2023-07-03 15:22:53 -0500

    The function to validate the administrator account is created.

--- config/Auth.php
+++ config/Auth.php
@@ -11,6 +11,19 @@ function inicioSesion($usuario, $clave, $con) {
     } else {
         return 'El usuario y/o clave son incorrectas';
     }
-} 
+}
+
+function inicioSesionAdmin($usuario, $clave, $con) {
+    $sql = $con->prepare("SELECT id, usuario FROM usuarios WHERE usuario=? AND clave=?");   
+    $sql->execute([$usuario, $clave]); 
+    if ($row = $sql->fetch(PDO::FETCH_ASSOC)) {  
+        $_SESSION['used_id'] = $row['id'];
+        $_SESSION['used_name'] = $row['usuario'];
+        header("Location: admin.php");
+        exit;
+    } else {
+        return 'El usuario y/o clave son incorrectas';
+    }
+}
 
 ?>
