e8a4826781c6b5062a775501ad77bb249783ddfa
Mateo Barona <theflacorex@gmail.com>
2023-07-03 15:21:19 -0500

    The structure of php and the function to validate the user has been added

---
+++ config/Auth.php
@@ -0,0 +1,16 @@
+<?php
+
+function inicioSesion($usuario, $clave, $con) {
+    $sql = $con->prepare("SELECT id, usuario FROM usuarios WHERE usuario=? AND clave=?");   
+    $sql->execute([$usuario, $clave]); 
+    if ($row = $sql->fetch(PDO::FETCH_ASSOC)) {  
+        $_SESSION['used_id'] = $row['id'];
+        $_SESSION['used_name'] = $row['usuario'];
+        header("Location: index.php");
+        exit;
+    } else {
+        return 'El usuario y/o clave son incorrectas';
+    }
+} 
+
+?>
