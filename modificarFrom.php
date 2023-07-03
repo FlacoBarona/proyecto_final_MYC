3c6ade15483c2ed7924208a46914239614f51d5b
Jairo Freire <jairofreireortiz10@gmail.com>
2023-07-03 18:04:29 -0500

    Header have been added into modificarForm

--- modificarFrom.php
+++ modificarFrom.php
@@ -40,6 +40,27 @@ if (isset($_POST['modificar'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+<header>
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a href="#" class="navbar-brand">
+          <strong>Tienda de videojuegos</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarHeader">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+          </ul>
+          <a href="inicioSesion.php" class="btn btn-success">
+            Cerrar sesion
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </header>
+  
 </body>
 </html>
