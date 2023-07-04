b9f0d2a2b6ad63e959ddac9439dd2092e78b12d6
Jairo Freire <jairofreireortiz10@gmail.com>
2023-07-03 22:10:59 -0500

    Function to add games to the car have been added

---
+++ clases/actualizar_carrito.php
@@ -0,0 +1,27 @@
+<?php
+    function agregar($id, $cantidad)
+    {
+        $res = 0;
+    
+        if ($id > 0 && $cantidad > 0 && is_numeric(($cantidad))) {
+            if (isset($_SESSION['carrito']['productos'][$id])) {
+                $_SESSION['carrito']['productos'][$id] = $cantidad;
+    
+                $db = new Database();
+                $con = $db->conectar();
+    
+                $sql = $con->prepare("SELECT precio, descuento FROM juegos WHERE id=? AND  activo=1 LIMIT 1");
+                $sql->execute([$id]);
+                $row = $sql->fetch(PDO::FETCH_ASSOC);
+                $precio = $row['precio'];
+                $descuento = $row['descuento'];
+                $precio_des = $precio - (($precio * $descuento) / 100);
+                $res = $cantidad * $precio_des;
+    
+                return $res;
+            }
+        }else{
+            return $res;
+        }
+    }
+?>
