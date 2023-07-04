d21c824b8743c211373f3c83497293eb438e56d9
Joel Espinoza <jespinoza5229@gmail.com>
2023-07-03 22:53:32 -0500

    Links are established for the video game store

--- detalles.php
+++ detalles.php
@@ -54,3 +54,39 @@ if ($id == '' || $token == '') {
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tienda Online</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a href="#" class="navbar-brand">
+          <strong>Tienda de videojuegos</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarHeader">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          </ul>
+          <a href="checkout.php" class="btn btn-primary">
+            Carrito <samp id="num_cart" class="badge bg-secondary"><?php echo $num_cart?></samp>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </header>
+    
+</body>
+</html>
