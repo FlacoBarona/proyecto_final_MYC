952911aa6e0d8db6be0c40bb13a5b9be367273eb
Jairo Freire <jairofreireortiz10@gmail.com>
2023-07-03 23:18:25 -0500

    An update function was added to the script

--- checkout.php
+++ checkout.php
@@ -147,7 +147,38 @@ if ($productos != null) {
             let buttonElimina = eliminaModal.querySelector('.modal-footer #btn-elimina')
             buttonElimina.value = id
         })
-        
+
+        function actualizaCantidad(cantidad, id) {
+            let url = 'clases/actualizar_carrito.php'
+            let formData = new FormData()
+            formData.append('action', 'agregar')
+            formData.append('id', id)
+            formData.append('cantidad', cantidad)
+
+            fetch(url, {
+                    method: 'POST',
+                    body: formData,
+                    mode: 'cors'
+                }).then(response => response.json())
+                .then(data => {
+                    if (data.ok) {
+
+                        let divsubtotal = document.getElementById('subtotal_' + id)
+                        divsubtotal.innerHTML = data.sub
+
+                        let total = 0.00
+                        let list = document.getElementsByName('subtotal[]')
+
+                        for (let i = 0; i < list.length; i++) {
+                            total += parseFloat(list[i].innerHTML.replace(/[$,]/g, ''))
+                        }
+                        total = new Intl.NumberFormat('en-US', {
+                            minimumFractionDigits: 2
+                        }).format(total)
+                        document.getElementById('total').innerHTML = '<?php echo MONEDA; ?>' + total
+                    }
+                })
+        }
     </script>
 </body>
 </html>
