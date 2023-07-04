e0af7d4e68d225348849fbb6fbb476798a26fd72
Jairo Freire <jairofreireortiz10@gmail.com>
2023-07-03 22:52:39 -0500

    Header have been added

--- checkout.php
+++ checkout.php
@@ -9,6 +9,25 @@
     <title>Online Store</title>
 </head>
 <body>
-    
+<header>
+        <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+                    <strong>Tienda de videojuegos</strong>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarHeader">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    </ul>
+                    <a class="btn btn-primary">
+                        Carrito <samp id="num_cart" class="badge bg-secondary"><?php echo $num_cart ?></samp>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </header>
 </body>
 </html>
