25ed05a8d231a8f1d3fb4213a9c472940036516e
Mateo Barona <theflacorex@gmail.com>
2023-07-03 22:23:27 -0500

    Credit card form available.

--- metodosPago.php
+++ metodosPago.php
@@ -80,13 +80,60 @@
             <div id="cancelar">
                 <a href="checkout.php" class="btn btn-lg btn-primary btn-login fw-bold text-uppercase" type="submit"> Cancelar </a>
             </div>
-
         </div>
-
     </form>
 
+    <form id="tarjeta" class="tarjeta">
+        <h2>Informacion de tarjeta</h2>
+        <div class="form-floating mb-3 col-md-2 col-lg-4 ">
+            <input type="number" class="form-control" id="num_tarjeta" name="num_tarjeta" placeholder="myusername" required>
+            <label for="usuario">Ingrese el numero de tarjeta</label>
+        </div>
+        <div class="form-floating mb-3 col-md-5 col-lg-4">
+            <input type="text" class="form-control" id="fecha_cad" name="fecha_cad" placeholder="myusername" required>
+            <label for="usuario">Fecha de caducidad 00/0000</label>
+        </div>
 
-    
+        <div class="form-floating mb-3 col-md-5 col-lg-3">
+            <input type="number" class="form-control" id="cod_tar" name="cod_tar" placeholder="myusername" required>
+            <label for="usuario">Codigo de seguridad</label>
+        </div>
+        <h2>Informe de facturacion</h2>
+        <div class="form-floating mb-3 ">
+            <input type="text" class="form-control" id="nombre" name="nombre" placeholder="myusername" required>
+            <label for="usuario">Ingrese su nombre</label>
+        </div>
+        <div class="form-floating mb-3 ">
+            <input type="text" class="form-control" id="apellido" name="apellido" placeholder="myusername" required>
+            <label for="usuario">Ingrese sus apellidos</label>
+        </div>
+        <div class="form-floating mb-3 ">
+            <input type="text" class="form-control" id="localidad" name="localidad" placeholder="myusername" required>
+            <label for="usuario">Ingrese la localidad</label>
+        </div>
+        <div class="form-floating mb-3 ">
+            <input type="text" class="form-control" id="direccion" name="direccion" placeholder="myusername" required>
+            <label for="usuario">Ingrese su direccion</label>
+        </div>
+        <div class="form-floating mb-3 ">
+            <input type="text" class="form-control" id="pais" name="pais" placeholder="myusername" required>
+            <label for="usuario">Ingrese su pais</label>
+        </div>
+        <div class="form-floating mb-3 ">
+            <input type="number" class="form-control" id="telf" name="telf" placeholder="myusername" required>
+            <label for="usuario">Ingrese su telefono</label>
+        </div>
+        <form action="compra.php">
+        <div id="paga">
+                <a href="compra.php" class="btn btn-lg btn-primary btn-login fw-bold text-uppercase" type="submit"> Pagar </a>
+            </div>
+            <br/>
+            <div id="cancelar">
+                <a href="checkout.php" class="btn btn-lg btn-primary btn-login fw-bold text-uppercase" type="submit"> Cancelar </a>
+            </div>
+        </form>
+    </form>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/funciones.js"></script>
 </body>
