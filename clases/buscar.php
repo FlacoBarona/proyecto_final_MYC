c3a8dbc4ee3e1c58065d9421246b57e593b65ca8
Joseph Cargua <josepcargua94@gmail.com>
2023-07-03 22:49:50 -0500

    Added code which will help to fetch the specific data from the database in buscar.php

--- clases/buscar.php
+++ clases/buscar.php
@@ -20,3 +20,24 @@ if($campo != null){
         <td></td>
     </tr>";
 }
+$resultado = $con->query($sql);
+if ($resultado->num_rows > 0) {
+$fila = $resultado->fetch_assoc();
+        $salida .=
+            "<tr>
+                <td>" . $fila['nombre'] . "</td>
+                <td>" . $fila['descripcion'] . "</td>
+                <td>" . $fila['precio'] . "</td>
+                <td>" . $fila['descuento'] . ' %'. "</td>
+                <td>" . $fila['id_categoria'] . "</td>
+                <td>". ' <a href="index.php" class="btn btn-primary">Ver detalles</a>'. "</td>
+            </tr>";
+
+} else {
+    $salida .= "NO HAY COINSIDENCIA";
+}
+
+echo json_encode($salida, JSON_UNESCAPED_UNICODE);
+$con->close();
+
+?>
