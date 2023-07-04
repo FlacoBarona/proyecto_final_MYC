3e1eb5c36f9bfbe90835c0aa578ddd0dc463c4d6
Jairo Freire <jairofreireortiz10@gmail.com>
2023-07-03 23:17:03 -0500

    A script was created to capture the delete modal.

--- checkout.php
+++ checkout.php
@@ -138,5 +138,16 @@ if ($productos != null) {
             </div>
         </div>
     </div>
+
+    <script>
+        let eliminaModal = document.getElementById('eliminaModal')
+        eliminaModal.addEventListener('show.bs.modal', function(event) {
+            let button = event.relatedTarget
+            let id = button.getAttribute('data-bs-id')
+            let buttonElimina = eliminaModal.querySelector('.modal-footer #btn-elimina')
+            buttonElimina.value = id
+        })
+        
+    </script>
 </body>
 </html>
