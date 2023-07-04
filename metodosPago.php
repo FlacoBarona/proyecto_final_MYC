4479bd4496270a80608ee154d978d9636ac96a14
Mateo Barona <theflacorex@gmail.com>
2023-07-03 22:16:16 -0500

    The header has been added.

--- metodosPago.php
+++ metodosPago.php
@@ -19,6 +19,23 @@
     <link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
-    
+<header>
+        <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+                    <strong>Tienda de videojuegos</strong>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarHeader">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+                    <a href="inicioSesion.php" class="btn btn-success">
+                        Log Out
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
 </body>
 </html>
