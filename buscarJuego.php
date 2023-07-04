d738d04e44e38464e76fd6ec25aede37947844bd
Joseph Cargua <josepcargua94@gmail.com>
2023-07-03 22:08:44 -0500

    Added header in buscarJuego.php

--- buscarJuego.php
+++ buscarJuego.php
@@ -11,6 +11,27 @@
 </head>
 
 <body>
+<header>
+        <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+                    <strong>Tienda de videojuegos</strong>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarHeader">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    </ul>
+                    <a href="inicioSesion.php" class="btn btn-success">
+                        Cerrar sesion
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </header>
 
 </body>
 </html>
