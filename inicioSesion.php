e8234fabac92c9e6a0ee9f5cba630ac2c5358ed5
Mateo Barona <theflacorex@gmail.com>
2023-07-16 17:46:01 -0500

    CM-16 The footer was added on all pages, including the styles and boostrap

--- inicioSesion.php
+++ inicioSesion.php
@@ -32,6 +32,7 @@ if (!empty($_POST)) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../Tienda_online/CSS/estyls.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="CSS/styles.css">
     <title>SIGN IN</title>
 </head>
 <body>
@@ -63,5 +64,28 @@ if (!empty($_POST)) {
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<footer>
+    <div class="footer-content">
+      <div class="footer-links">
+        <a href="index.php"><i class="fas fa-home"></i> Inicio</a>                        
+      </div>
+      <div class="footer-info">
+        <p><i class="fas fa-envelope"></i> Contacto: jespinoza5229@gmail.com</p>
+        <p><i class="fas fa-map-marker-alt"></i> Dirección: Ambato, UTA</p>
+        <b></b>
+      </div>
+      
+      <div class="footer-content">
+        
+        <div class="social-icons">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>          
+        </div>
+      </div>
+    </div>
+    <p class="footer-copyright">© 2023 Grupo 4. Todos los derechos reservados.</p>  
+</footer>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </body>
 </html>
