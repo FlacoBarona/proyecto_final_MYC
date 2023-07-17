689e2d9a49cffe8617c39ff98dbbc329d3758eff
Jairo Freire <jairofreireortiz10@gmail.com>
2023-07-17 00:29:07 -0500

    CM-10 A function was created to check if the image exists.

--- JS/script.js
+++ JS/script.js
@@ -19,3 +19,10 @@ function searchGames() {
   
     xhr.send();
   }
+
+  function fileExists(url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status !== 404;
+  }
